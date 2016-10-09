322fe30ca182d5340897a4171a0c903851b3a0aa
techmore <sdolbec1@gmail.com>
2016-10-09 16:13:16 -0400

    Update Nuke_Me_Installer.sh

--- Nuke_Me_Installer.sh
+++ Nuke_Me_Installer.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+apt -y install pv python-pisa python-html5lib
 echo ""
-mkdir ~/Desktop/TM_Nuke
-mkdir ~/Desktop/TM_Nuke/Logos
-mkdir ~/Desktop/TM_Nuke/Sanitation_Logs
+mkdir -p ~/Desktop/TM_Nuke ~/Desktop/TM_Nuke/Logos ~/Desktop/TM_Nuke/Sanitation_Logs
 
 cd /tmp
 # UPDATE HERE WHEN INSTALL FILES CHANGE
-wget https://www.dropbox.com/s/9kpqp52k36wjsyg/Nuke_Install_v3.zip
-unzip Nuke_Install_v3.zip
-sleep .2
-cd Nuke_Install_v3
+# wget https://www.dropbox.com/s/9kpqp52k36wjsyg/Nuke_Install_v3.zip
+# unzip Nuke_Install_v3.zip
+# sleep .2
+# cd Nuke_Install_v3
 
-sudo chmod 775 TM_Nuke.sh
-sudo chmod 775 Single_Pass.sh
-sudo chmod 775 Triple_Pass.sh
+sudo chmod 775 TM_Nuke.sh Single_Pass.sh Triple_Pass.sh
 
-sudo dpkg -i pv_1.2.0-1_amd64.deb
-sudo dpkg -i python-html5lib_0.999-2_all.deb
-sudo dpkg -i python-support_1.0.15_all.deb
-sudo dpkg -i python-pypdf_1.13-1_all.deb
-sudo dpkg -i python-reportlab_3.0-1build1_all.deb
-sudo dpkg -i python-pisa_3.0.32-1build1_all.deb
+# sudo dpkg -i pv_1.2.0-1_amd64.deb
+# sudo dpkg -i python-html5lib_0.999-2_all.deb
+# sudo dpkg -i python-support_1.0.15_all.deb
+# sudo dpkg -i python-pypdf_1.13-1_all.deb
+# sudo dpkg -i python-reportlab_3.0-1build1_all.deb
+# sudo dpkg -i python-pisa_3.0.32-1build1_all.deb
 
 mv TM_Nuke.sh ~/Desktop/TM_Nuke
 mv Single_Pass.sh ~/Desktop/TM_Nuke
@@ -38,6 +40,6 @@ echo "Terminal=true" >> tm-nuke.desktop
 echo "Type=Application" >> tm-nuke.desktop
 echo "StartupNotify=true" >> tm-nuke.desktop
 
-sudo mv tm-nuke.desktop /usr/share/applications/
-sudo chown root /usr/share/applications/tm-nuke.desktop
-sudo chgrp root /usr/share/applications/tm-nuke.desktop 
+mv tm-nuke.desktop /usr/share/applications/
+chown root /usr/share/applications/tm-nuke.desktop
+chgrp root /usr/share/applications/tm-nuke.desktop 
