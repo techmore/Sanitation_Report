179ce247a65f76b11208483a6264ed9511e44a7f
techmore <sdolbec1@gmail.com>
2016-10-11 00:11:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,16 +17,17 @@ mkdir -p $HOME/Downloads/TM_Nuke $HOME/Downloads/TM_Nuke/Logos $HOME/Downloads/T
 
 sudo chmod 775 TM_Nuke.sh Single_Pass.sh Triple_Pass.sh
 
-dpkg -i pv_1.6.0-1_amd64.deb
-dpkg -i python-six_1.10.0-3_all.deb
-dpkg -i python-html5lib_0.999-4_all.deb
-dpkg -i python-reportlab-accel_3.3.0-1_amd64.deb
-dpkg -i python-reportlab_3.3.0-1_all.deb
-dpkg -i python-pil_3.1.2-0ubuntu1_amd64.deb
-dpkg -i python-imaging_3.1.2-0ubuntu1_all.deb
-dpkg -i python-pypdf_1%3a1.13-3_all.deb
-dpkg -i python-pkg-resources_20.7.0-1_all.deb
-dpkg -i python-pisa_3.0.32-3_all.deb
+ dpkg -i pv_1.6.0-1_amd64.deb
+
+# dpkg -i python-six_1.10.0-3_all.deb
+# dpkg -i python-html5lib_0.999-4_all.deb
+# dpkg -i python-reportlab-accel_3.3.0-1_amd64.deb
+# dpkg -i python-reportlab_3.3.0-1_all.deb
+# dpkg -i python-pil_3.1.2-0ubuntu1_amd64.deb
+# dpkg -i python-imaging_3.1.2-0ubuntu1_all.deb
+# dpkg -i python-pypdf_1%3a1.13-3_all.deb
+# dpkg -i python-pkg-resources_20.7.0-1_all.deb
+# dpkg -i python-pisa_3.0.32-3_all.deb
 rm pv_1.6.0-1_amd64.deb python-six_1.10.0-3_all.deb python-html5lib_0.999-4_all.deb python-reportlab-accel_3.3.0-1_amd64.deb python-reportlab_3.3.0-1_all.deb python-pil_3.1.2-0ubuntu1_amd64.deb python-imaging_3.1.2-0ubuntu1_all.deb python-pypdf_1%3a1.13-3_all.deb python-pkg-resources_20.7.0-1_all.deb python-pisa_3.0.32-3_all.deb
 # sudo dpkg -i python-support_1.0.15_all.deb
 
